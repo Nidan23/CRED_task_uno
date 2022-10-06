d57b8d953bd2d74d907cf53cf9f8aa8801dd079d
Nidan23 <dev.sulkowski@gmail.com>
2022-10-06 18:24:00 +0200

    [ADD] Added test class

---
+++ src/test/java/FileDifferentiatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import service.FileService;
+
+public class FileDifferentiatorTest {
+    private final FileService fileService = FileService.getInstance();
+
+
+    @Test
+    public void testFakeGif() {
+
+    }
+}
