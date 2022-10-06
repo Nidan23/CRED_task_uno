2e18b74721a753ced59127c4e0700ba884ea11f9
Nidan23 <dev.sulkowski@gmail.com>
2022-10-06 18:18:36 +0200

    [ADD] Added test TxtFileValues

---
+++ src/main/java/domain/TxtFileValues.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class TxtFileValues {
+    public static int tab = 9;
+    public static int newLine = 10;
+    public static int newLine2 = 13;
+    public static int forbiddenRangeBottom = 0;
+    public static int forbiddenRangeTop = 31;
+
+    public static int maxTxtASCIIByteValue = 127;
+}
