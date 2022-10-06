f74d2079a99eec9ca7f1a83419f3d8ff25eabf83
Nidan23 <dev.sulkowski@gmail.com>
2022-10-06 18:23:47 +0200

    [ADD] Added dependencies & fixed singleton invocation

--- src/main/java/FileDifferentiator.java
+++ src/main/java/FileDifferentiator.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class FileDifferentiator {
+    private static final FileService fileService = FileService.getInstance();
 
     public static void main(String[] args) throws IOException {
-        FileService fileService = FileService.getInstance();
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Write your file path");
         String filePath = scanner.nextLine();
-        System.out.println(fileService.validateFile(filePath));
+        System.out.println(FileDifferentiator.fileService.validateFile(filePath));
     }
 }
